6848b5e361f0a9a915840b3d836c682ac92e514d
hello_kai_di <michael.li@aiello.ai>
2020-05-30 17:05:37 +0800

    step2

--- week07/layout.js
+++ week07/layout.js
@@ -110,4 +110,65 @@ function layout(element) {
     crossSign = 1;
   }
 
+  // 特例
+  let isAutoMainSize = false;
+  if (!style[mainSize]) { // auto sizing
+    elementStyle[mainSize] = 0;
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      if (itemStyle[mainSize] !== null || itemStyle[mainSize] !== (void 0)) {
+        elementStyle[mainSize] = elementStyle[mainSize] + itemStyle[mainSize];
+      }
+    }
+    isAutoMainSize = true;
+    // style.flexWrap = 'nowrap'
+  }
+  // 行
+  let flexLine = [];
+  let flexLines = [flexLine];
+  // 行內空白
+  let mainSpace = elementStyle[mainSize];
+  let crossSpace = 0;
+
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    let itemStyle = getStyle(item);
+
+    if (itemStyle[mainSize] === null) {
+      itemStyle[mainSize] = 0;
+    }
+
+    if (itemStyle.flex) {
+      flexLine.push(item);
+    } else if (style.flexWrap === 'nowrap' && isAutoMainSize) {
+      mainSpace -= itemStyle[mainSize];
+      if (itemStyle[crossSize] !== null && itemStyle[crossSize] !== (void 0)) {
+        crossSpace = Math.max(crossSpace, itemStyle[crossSize]);
+      }
+      flexLine.push(item);
+    } else {
+      if (itemStyle[mainSize] > style[mainSize]) {
+        itemStyle[mainSize] = style[mainSize];
+      }
+      if (mainSpace < itemStyle[mainSize]) {
+        flexLine.mainSpace = mainSpace;
+        flexLine.crossSpace = crossSpace;
+        flexLine = [item];
+        flexLines.push(flexLine);
+        mainSpace = style[mainSize];
+        crossSpace = 0;
+      } else {
+        flexLine.push(item);
+      }
+      if (itemStyle[crossSize] !== null && itemStyle[crossSize] !== (void 0)) {
+        crossSpace = Math.max(crossSpace, itemStyle[crossSize]);
+      }
+      mainSpace -= itemStyle[mainSize];
+    }
+    flexLine.mainSpace = mainSpace;
+
+    console.log(items);
+  }
 }
+
+module.exports = layout;
